ddbedde456c7a8147d68021b32fc6da68e40c46b
JeanCar <jeancarpc@gmail.com>
2019-08-21 16:01:51 -0300

    Commit testando botoes

--- src/Tela.java
+++ src/Tela.java
@@ -1,12 +1,48 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class Tela extends JFrame{
+public class Tela extends JFrame implements ActionListener{
+    
+    JButton[][] botoes = new JButton[3][3];
+    
     public Tela(){
         super("Jogo da velha - Tec. Desenv. de jogos Digitais JEAN CARLOS DE ALMEIDA");
         this.setVisible(true);
+        this.setLayout(new GridLayout(3, 3));
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setExtendedState(MAXIMIZED_BOTH);
+        
+        for (int i=0; i< 3; i++){
+            for (int j = 0; j < 3; j++){
+                botoes[i][j] = new JButton();
+                botoes[i][j].addActionListener(this);
+                this.add(botoes[i][j]);
+            }
+        }
     }
+
+    /*@Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }*/
+    public void actionPerformed(ActionEvent e){
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                if(e.getSource() == botoes[i][j]){
+                    botoes[i][j].setText("Teste");
+                }
+            }
+        }
+    }
+
     
+
+
+
     
 }
