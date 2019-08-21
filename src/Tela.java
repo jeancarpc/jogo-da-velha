fd265ea0f5e0f01ed16262082b937d736675eec2
JeanCar <jeancarpc@gmail.com>
2019-08-21 15:02:23 -0300

    Commit inicial

---
+++ src/Tela.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Tela extends JFrame{
+    public Tela(){
+        super("Jogo da velha - Tec. Desenv. de jogos Digitais JEAN CARLOS DE ALMEIDA");
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setExtendedState(MAXIMIZED_BOTH);
+    }
+    
+    
+}
